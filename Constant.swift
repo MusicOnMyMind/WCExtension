d470f61c7a7c41bc19b37bd92ae899e5c6d19d16
Mr.He <hewc@MrHedeMacBook-Pro.local>
2019-10-22 16:46:20 +0800

    test first commit

---
+++ Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  WCExtension
+//
+//  Created by Mr.He on 2019/10/22.
+//  Copyright © 2019 extension. All rights reserved.
+//
+
+import UIKit
+
+let wc_keyWindow = UIApplication.shared.windows.first!
+let wc_screenWidth = UIScreen.main.bounds.width
+let wc_screenHeight = UIScreen.main.bounds.height
+let wc_userDefault = UserDefaults.standard
+let wc_notificationCenter = NotificationCenter.default
